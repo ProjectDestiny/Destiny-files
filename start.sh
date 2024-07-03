7e2dc5ef39c61e681aa252bc042971531a1630dd
Rose <149821229+ProjectDestiny@users.noreply.github.com>
2024-07-03 23:15:54 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,31 @@
-#Downloads
 curl -s -o login.sh -L "https://github.com/ProjectDestiny/Destiny-files/edit/main/start.sh"
-#disable spotlight indexing
 sudo mdutil -i off -a
-#Create new account
+echo "----------------- ADDING USER -----------------"
 sudo useradd -m -s /bin/bash destiny && echo "destiny:password" | sudo chpasswd
 sudo usermod -aG sudo destiny
 cd ~
 mkdir Moujira
-#Fix
-sudo /usr/sbin/systemsetup -getremotelogin
-sudo /usr/sbin/systemsetup -setremotelogin on
-#Enable VNC
+echo "-----------------------------------------------"
+echo "DONE!!"
+echo "----------------- ALLOWING VNC -------------------"
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
-#Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-#install brew
-sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install node
-#verifying npm available
-npm -v
-#install localtunnel
-curl https://loca.lt/mytunnelpassword
-npm install -g localtunnel
-lt --port 5090
-#watermark
-echo Script by the one and only MOUJIRAAAA!!!
+echo "--------------------------------------------------"
+echo "DONE!!"
+echo "------------------ IP ADDRESS ------------------"
+curl ifconfig.co
+echo "------------------------------------------------"
+echo "*** REMEMBER THIS!! ***"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
+git --version
+brew install tiger-vnc
+vncserver :1
+git clone https://github.com/novnc/noVNC.git
+cd noVNC
+sudo chmod +x launch.sh
+./utils/launch.sh --vnc localhost:5901
+
