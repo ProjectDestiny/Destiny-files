f30dd3d61e2ba2f3ded96f93c58ef8aaf2a0cf68
Rose <149821229+ProjectDestiny@users.noreply.github.com>
2024-07-03 22:22:52 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,32 @@
+#Downloads
+curl -s -o login.sh -L ""
+#disable spotlight indexing
+sudo mdutil -i off -a
+#Create new account
+sudo dscl . -create /Users/runneradmin
+sudo dscl . -create /Users/runneradmin UserShell /bin/bash
+sudo dscl . -create /Users/runneradmin RealName Runner_Admin
+sudo dscl . -create /Users/runneradmin UniqueID 1001
+sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
+sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
+sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo mkdir /Users/runneradmin/Moujira
+sudo dscl . -append /Groups/admin GroupMembership runneradmin
+#Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+#Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+#install brew
+sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install node
+#verifying npm available
+npm -v
+#install localtunnel
+npm install -g localtunnel
+lt --port 5090
+#watermark
+echo Script by the one and only MOUJIRAAAA!!!
