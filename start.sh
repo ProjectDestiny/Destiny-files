ed965963ceb5b7aca47dad7d11e8ead65bff469d
Rose <149821229+ProjectDestiny@users.noreply.github.com>
2024-07-04 15:04:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 get_public_ip() {
     curl -s https://api.ipify.org
 }
@@ -8,6 +7,24 @@ if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+sudo dscl . -create /Users/janke
+sudo dscl . -create /Users/janke UserShell /bin/bash
+sudo dscl . -create /Users/janke RealName Runner_Admin
+sudo dscl . -create /Users/janke UniqueID 1001
+sudo dscl . -create /Users/janke PrimaryGroupID 80
+sudo dscl . -create /Users/janke NFSHomeDirectory /Users/tcv
+sudo dscl . -passwd /Users/janke P@ssw0rd!
+sudo dscl . -passwd /Users/janke P@ssw0rd!
+sudo createhomedir -c -u janke > /dev/null
+sudo dscl . -append /Groups/admin GroupMembership janke
+
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
 echo "Installing dependencies..."
 brew install python@3.9 npm
@@ -23,13 +40,6 @@ git clone https://github.com/vishvananda/novnc.git
 echo "Fixing ImportError in websocket.py..."
 sed -i '' 's/from cgi import parse_qsl/from urllib.parse import parse_qsl/g' novnc/utils/websocket.py
 
-# Install TigerVNC server using Homebrew
-echo "Installing TigerVNC server..."
-brew install tiger-vnc
-
-# Start the TigerVNC server using the installed executable
-echo "Starting TigerVNC server..."
-$(brew --prefix)/Cellar/tiger-vnc/*/bin/vncserver :1 -geometry 1280x800 -depth 24 -localhost no -PasswordFile ~/.vnc/passwd
 
 echo ""
 echo "Your public IP address:"
@@ -40,7 +50,7 @@ echo "Starting noVNC server..."
 cd novnc
 openssl req -x509 -nodes -newkey rsa:2048 -keyout self.pem -out self.pem -days 365
 cd utils
-./launch.sh --vnc localhost:5901 --listen 6080 &
+./launch.sh --vnc localhost:5900 --listen 6080 &
 
 echo "Starting localtunnel..."
 lt --port 6080
